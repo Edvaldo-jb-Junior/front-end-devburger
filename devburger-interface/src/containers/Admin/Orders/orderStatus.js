77983def2907e586712f7469a3296ce061d6fd88
Edvaldo-jb-Junior <juniorb2325@gmail.com>
2026-03-18 12:18:08 -0300

    react-select-update-orders

---
+++ devburger-interface/src/containers/Admin/Orders/orderStatus.js
@@ -0,0 +1,26 @@
+export const orderStatusOptions = [
+    {
+        id: 0, 
+        value: 'Todos', 
+        label: 'Todos'},
+    {
+        id: 1, 
+        value: 'Pedido Realizado', 
+        label: 'Pedido Realizado'},
+    {
+        id: 2, 
+        value: 'Em Preparação', 
+        label: 'Em Preparação'},
+    {
+        id: 3, 
+        value: 'pedido Pronto', 
+        label: 'pedido Pronto'},
+    {
+        id: 4, 
+        value: 'pedido á Caminho', 
+        label: 'pedido á Caminho'},
+    {
+        id: 5, 
+        value: 'pedido Entregue', 
+        label: 'pedido Entregue'}
+];
--- devburger-interface/src/containers/Admin/Orders/row.jsx
+++ devburger-interface/src/containers/Admin/Orders/row.jsx
@@ -11,11 +11,19 @@ import TableRow from '@mui/material/TableRow';
 import Typography from '@mui/material/Typography';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import { formatDate } from '../../../utils/formatDate';
+import { ProductImg, SelectStatus } from './styles';
+import { orderStatusOptions } from './orderStatus';
+import { api } from '../../../services/api';
 
 
  export function Row(props) {
   const { row } = props;
   const [open, setOpen] = useState(false);
+
+  async function newStatusOrder(id, status) {
+    await api.put(`orders/${id}`, { status })
+  }
 
   return (
     <>
@@ -30,39 +38,45 @@ import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
           </IconButton>
         </TableCell>
         <TableCell component="th" scope="row">
-          {row.name}
+          {row.orderId}
         </TableCell>
-        <TableCell align="right">{row.calories}</TableCell>
-        <TableCell align="right">{row.fat}</TableCell>
-        <TableCell align="right">{row.carbs}</TableCell>
-        <TableCell align="right">{row.protein}</TableCell>
+        <TableCell >{row.name}</TableCell>
+        <TableCell >{formatDate(row.data)}</TableCell>
+        <TableCell >
+            <SelectStatus 
+                options={ orderStatusOptions.filter((status) => status.id !== 0)} 
+                placeholder='Status'
+                defaultValue={ orderStatusOptions.find((status) => status.value === row.status)}
+                onChange={(status) => newStatusOrder(row.orderId, status.value)}
+                />
+        </TableCell>
       </TableRow>
       <TableRow>
         <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
           <Collapse in={open} timeout="auto" unmountOnExit>
             <Box sx={{ margin: 1 }}>
               <Typography variant="h6" gutterBottom component="div">
-                History
+                Pedido
               </Typography>
               <Table size="small" aria-label="purchases">
                 <TableHead>
                   <TableRow>
-                    <TableCell>Date</TableCell>
-                    <TableCell>Customer</TableCell>
-                    <TableCell align="right">Amount</TableCell>
-                    <TableCell align="right">Total price ($)</TableCell>
+                    <TableCell>Quantidade</TableCell>
+                    <TableCell>Produto</TableCell>
+                    <TableCell >Categoria</TableCell>
+                    <TableCell >Imagem do Produto</TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {row.history.map((historyRow) => (
-                    <TableRow key={historyRow.date}>
+                  {row.products.map((product) => (
+                    <TableRow key={ product.id }>
                       <TableCell component="th" scope="row">
-                        {historyRow.date}
+                        {product.id}
                       </TableCell>
-                      <TableCell>{historyRow.customerId}</TableCell>
-                      <TableCell align="right">{historyRow.amount}</TableCell>
-                      <TableCell align="right">
-                        {Math.round(historyRow.amount * row.price * 100) / 100}
+                      <TableCell>{product.name}</TableCell>
+                      <TableCell >{product.category}</TableCell>
+                      <TableCell >
+                        <ProductImg src={product.url} alt={product.name}/>
                       </TableCell>
                     </TableRow>
                   ))}
@@ -78,18 +92,19 @@ import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 Row.propTypes = {
   row: PropTypes.shape({
-    calories: PropTypes.number.isRequired,
-    carbs: PropTypes.number.isRequired,
-    fat: PropTypes.number.isRequired,
-    history: PropTypes.arrayOf(
+    orderId: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    data: PropTypes.string.isRequired,
+    products: PropTypes.arrayOf(
       PropTypes.shape({
-        amount: PropTypes.number.isRequired,
-        customerId: PropTypes.string.isRequired,
-        date: PropTypes.string.isRequired,
+        id: PropTypes.number.isRequired,
+        category: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        price: PropTypes.number.isRequired,
+        quantity: PropTypes.number.isRequired,
+        url: PropTypes.string.isRequired,
       }),
     ).isRequired,
-    name: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
-    protein: PropTypes.number.isRequired,
+    staus: PropTypes.string.isRequired,
   }).isRequired,
 };
