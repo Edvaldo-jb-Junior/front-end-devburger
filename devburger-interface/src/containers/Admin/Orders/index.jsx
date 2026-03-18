9752c943dd76de9496393c53a7145e2655aa3c9e
Edvaldo-jb-Junior <juniorb2325@gmail.com>
2026-03-18 10:56:01 -0300

    start-order-table

--- devburger-interface/src/containers/Admin/Orders/index.jsx
+++ devburger-interface/src/containers/Admin/Orders/index.jsx
@@ -1,9 +1,70 @@
 
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Row } from './row';
+import { useEffect, useState } from 'react';
+import { api } from '../../../services/api'
 
-export function Orders(){
-    return(
-        <div>
-            <h1>pedidos</h1>
-        </div>
-    )
+
+export function  Orders() {
+
+  const [orders, setOrders] = useState([]);
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    async function loadOrders() {
+      const { data } =  await api.get('/orders')
+      
+      setOrders(data);
+     
+    }
+
+    loadOrders();
+  }, []);
+
+  useEffect(() => {
+    const newRows = orders.map(order => createData(order));
+
+    setRows(newRows)
+  }, [orders]);
+
+  function createData(order) {
+    return {
+      name: order.user.name,
+      orderId: order._id,
+      data: order.createdAt,
+      status: order.status,
+      products: order. products,
+   
+  };
 }
+
+
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="collapsible table">
+        <TableHead>
+          <TableRow>
+            <TableCell />
+            <TableCell >Pedido</TableCell>
+            <TableCell >Cliente</TableCell>
+            <TableCell >Data do Pedido</TableCell>
+            <TableCell >Status</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          //{rows.map((row) => (
+            <Row key={row._id} row={row} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+
